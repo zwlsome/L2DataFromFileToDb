df8ea9bfa40612e6eb1e6e32bbab7b5d3afd37fc
zwlsome <332923023@qq.com>
2019-01-02 09:58:07 +0800

    null

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "mysql/my_global.h"
+#include "mysql/mysql.h"
+
+int main()
+{
+	MYSQL* tpSql = mysql_init(NULL);
+
+	if(NULL == tpSql)
+	{
+		printf("Failed to mysql_init()");
+		return -1;
+	}
+
+	if(NULL == mysql_real_connect(tpSql, "192.168.1.12", "root", "abc123", "QT2", 3306, NULL, 0))
+	{
+		mysql_close(tpSql);
+		printf("Failed to mysql_real_connect(): Error=%s\r\n", mysql_error(tpSql));
+		return -1;
+	}
+
+	printf("Connect ok\r\n");
+
+	char tCh = getchar();
+
+	mysql_close(tpSql);
+	printf("Closed\r\n");
+
+	return 0;
+}
+
